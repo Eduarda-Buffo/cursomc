2f2fd383a402dcd8c441a192707b3c1e53b28123
Eduarda Buffo <duda.buffo2@gmail.com>
2021-08-11 14:12:05 -0300

    Produto e associacao muitos pra muitos

--- src/main/java/com/eduarda/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/eduarda/cursomc/repositories/ProdutoRepository.java
@@ -4,10 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
-import com.eduarda.cursomc.domain.Categoria;
+import com.eduarda.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
 
 
