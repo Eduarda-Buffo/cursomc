caf4f8830568267a9209d0aa1c532f61d33ab40b
Eduarda Buffo <duda.buffo2@gmail.com>
2021-08-16 13:08:39 -0300

    Validação sintática comBean Validation

--- src/main/java/com/eduarda/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduarda/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.eduarda.cursomc.domain.Categoria;
+import com.eduarda.cursomc.dto.CategoriaDTO;
 import com.eduarda.cursomc.repositories.CategoriaRepository;
 import com.eduarda.cursomc.resources.exceptions.DataIntegrityException;
 import com.eduarda.cursomc.services.esceptions.ObjectNotFoundException;
@@ -57,6 +58,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
