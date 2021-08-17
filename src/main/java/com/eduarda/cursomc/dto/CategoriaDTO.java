80612d250bf64999a6a57138cfb2567bfb110683
Eduarda Buffo <duda.buffo2@gmail.com>
2021-08-17 10:00:34 -0300

    Validação customizada: CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/eduarda/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/eduarda/cursomc/dto/CategoriaDTO.java
@@ -2,27 +2,33 @@ package com.eduarda.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.eduarda.cursomc.domain.Categoria;
+import com.eduarda.cursomc.domain.Cliente;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-
-	private Integer id;
 	
+	private Integer id;
+
 	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
+	private String email;
 
 	public CategoriaDTO() {
 	}
 
-	public CategoriaDTO(Categoria obj) {
+	public CategoriaDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();
+		email = obj.getEmail();
 	}
 
 	public Integer getId() {
@@ -39,5 +45,14 @@ public class CategoriaDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}	
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
