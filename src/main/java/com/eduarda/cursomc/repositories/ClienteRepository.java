66d335cbf65f0a64c20cccc01ab5d0b9b78d010f
Eduarda Buffo <duda.buffo2@gmail.com>
2021-08-17 10:08:11 -0300

    Validação costomizada:email não repetido na inserção de Cliente

--- src/main/java/com/eduarda/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/eduarda/cursomc/repositories/ClienteRepository.java
@@ -1,8 +1,8 @@
 package com.eduarda.cursomc.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import org.springframework.stereotype.Repository;
 
 import com.eduarda.cursomc.domain.Cliente;
@@ -11,5 +11,7 @@ import com.eduarda.cursomc.domain.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
