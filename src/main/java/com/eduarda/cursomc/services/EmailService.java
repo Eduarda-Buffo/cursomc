4f288e5b9925a291cfe6c76dbc371f2448335d82
Eduarda Buffo <duda.buffo2@gmail.com>
2021-08-19 14:26:23 -0300

    Email HTML

--- src/main/java/com/eduarda/cursomc/services/EmailService.java
+++ src/main/java/com/eduarda/cursomc/services/EmailService.java
@@ -1,5 +1,8 @@
 package com.eduarda.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.eduarda.cursomc.domain.Pedido;
@@ -9,4 +12,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
