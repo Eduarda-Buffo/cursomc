98663f60588de44d9f1d6d3d298ec52567a9e7e4
Eduarda Buffo <duda.buffo2@gmail.com>
2021-08-19 16:02:59 -0300

    Adcionando senha a Cliente

--- src/main/java/com/eduarda/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/eduarda/cursomc/dto/ClienteNewDTO.java
@@ -36,6 +36,9 @@ public class ClienteNewDTO implements Serializable {
 	private String cep;
 
 	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
@@ -147,5 +150,13 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 }
