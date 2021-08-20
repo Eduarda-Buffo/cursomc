547e67f1dd306fce61748e9fc93c02462be3a1b9
Eduarda Buffo <duda.buffo2@gmail.com>
2021-08-20 11:09:08 -0300

    Esqueci a senha

--- src/main/java/com/eduarda/cursomc/services/EmailService.java
+++ src/main/java/com/eduarda/cursomc/services/EmailService.java
@@ -2,9 +2,9 @@ package com.eduarda.cursomc.services;
 
 import javax.mail.internet.MimeMessage;
 
-
 import org.springframework.mail.SimpleMailMessage;
 
+import com.eduarda.cursomc.domain.Cliente;
 import com.eduarda.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -16,4 +16,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
